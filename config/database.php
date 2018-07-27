b6f23b7e1d36aee0f6ce7c7abd629c6f0c4c1d27
thanhhungmk92 <thanhhungmk92@gmail.com>
2018-07-27 22:58:29 +0700

    fix default

--- config/database.php
+++ config/database.php
@@ -1,11 +1,11 @@
 <?php
-
-return [
 $url = parse_url(getenv('DATABASE_URL'));
 $host = $url['host'] ?? null;
 $username = $url['user'] ?? null;
 $password = $url['pass'] ?? null;
 $database = substr($url['path'], 1) ?? null;
+return [
+
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
