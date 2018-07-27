efd598230536b68eba730ec0b74f6f70920f323a
thanhhungmk92 <thanhhungmk92@gmail.com>
2018-07-27 22:18:09 +0700

    aa

--- config/database.php
+++ config/database.php
@@ -1,7 +1,11 @@
 <?php
 
 return [
-
+$url = parse_url(getenv('DATABASE_URL'));
+$host = $url['host'] ?? null;
+$username = $url['user'] ?? null;
+$password = $url['pass'] ?? null;
+$database = substr($url['path'], 1) ?? null;
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -13,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql_production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -77,6 +81,18 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
         ],
+        'pgsql_production' => [
+    'driver' => 'pgsql',
+    'host' => $host,
+    'port' => env('DB_PORT', '5432'),
+    'database' => $database,
+    'username' => $username,
+    'password' => $password,
+    'charset' => 'utf8',
+    'prefix' => '',
+    'schema' => 'public',
+    'sslmode' => 'prefer',
+],
 
     ],
 
